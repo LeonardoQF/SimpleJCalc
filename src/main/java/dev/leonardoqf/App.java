73ac4a35672f74bd419c753ad7e6a9abe25b44a8
LeonardoQF <leoqf1234@outlook.com>
2026-09-08 22:19:44 -0300

    added buttons

--- src/main/java/dev/leonardoqf/App.java
+++ src/main/java/dev/leonardoqf/App.java
@@ -1,9 +1,18 @@
 package dev.leonardoqf;
 
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import dev.leonardoqf.view.*;
 
 public class App {
     public static void main(String[] args) {
+        try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         CalcFrame introFrame = new CalcFrame();
+        } catch(ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+            System.out.println("Error while getting system LAF");
+        }
     }
 }
--- src/main/java/dev/leonardoqf/view/CalcFrame.java
+++ src/main/java/dev/leonardoqf/view/CalcFrame.java
@@ -1,8 +1,8 @@
 package dev.leonardoqf.view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -19,16 +19,18 @@ public class CalcFrame extends JFrame {
     }
 
     //Buttons panel should be sized 500x420, the gap 500x30, and the screen 500x150.
+    //Buttons should have a margin from the sides of 25, and a distance between themselves of 18. The buttons should be sized x = 99.
 
     public void init() {
+        this.setLayout(new BorderLayout(25, 25));
         this.setTitle("SimpleJCalc");
         this.setSize(500, 600);
         this.getContentPane().setBackground(Color.GRAY);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setLayout(null);
 
-        this.add(buttonsPanel);
+
+        this.add(buttonsPanel, BorderLayout.SOUTH);
 
         this.setVisible(true);
     }
