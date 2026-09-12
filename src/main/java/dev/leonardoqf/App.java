8b5609ede9140a119f1b28b58cd7fcc4e5e1340a
LeonardoQF <leoqf1234@outlook.com>
2026-09-12 03:31:00 -0300

    isValidExpression method implemented

--- src/main/java/dev/leonardoqf/App.java
+++ src/main/java/dev/leonardoqf/App.java
@@ -5,6 +5,12 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import dev.leonardoqf.view.*;
 
+/*Instead of allowing the user to write a whole expression and then painstakingly parsing it,
+I could make it behave like a simple calculator: You type a value, press the operation button, then input the second value and = and BAM, you get the number.
+*/
+
+
+
 public class App {
     public static void main(String[] args) {
         try {
--- src/main/java/dev/leonardoqf/model/CalcModel.java
+++ src/main/java/dev/leonardoqf/model/CalcModel.java
@@ -1,9 +1,52 @@
 package dev.leonardoqf.model;
 
-public class CalcModel {
+import java.util.List;
+
+public class CalcModel implements ExpressionCalculator {
+
+    private final String VALID_CHARACTERS = "1234567890 ";
 
     public CalcModel() {
 
+    }
+
+    @Override
+    public boolean calculate(String exp) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+    }
+
+    //TODO should also verify whether an expression is legal
+    @Override
+    public boolean isValidExpression(String exp) {
+
+        List<Character> expChars = exp.chars().mapToObj(c -> (char) c).toList();
+
+        return expChars.stream().allMatch(c -> VALID_CHARACTERS.indexOf(c) >= 0);
+    }
+
+    @Override
+    public double sum(double num1, double num2) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'sum'");
+    }
+
+    @Override
+    public double subtract(double num1, double num2) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'subtract'");
+    }
+
+    @Override
+    public double multiply(double num1, double num2) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'multiply'");
+    }
+
+    @Override
+    public double divide(double num1, double num2) {
+        // TODO 
+        throw new UnsupportedOperationException("Unimplemented method 'divide'");
     }
 
 }
