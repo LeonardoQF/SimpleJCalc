af9aee70d81fb3d041825758c85265cba8afa1b0
LeonardoQF <leoqf1234@outlook.com>
2026-09-19 16:00:27 -0300

    make model stateful

--- src/main/java/dev/leonardoqf/model/CalcModel.java
+++ src/main/java/dev/leonardoqf/model/CalcModel.java
@@ -6,22 +6,58 @@ import dev.leonardoqf.model.factories.MathStrategyFactory;
 
 public class CalcModel {
 
-    private final String VALID_CHARACTERS = "1234567890 ";
+    private static final String VALID_CHARACTERS = "1234567890 ";
     private final MathStrategyFactory strategyFactory;
+
+    private double num1;
+    private double num2;
 
     public CalcModel(MathStrategyFactory strategyFactory) {
         this.strategyFactory = strategyFactory;
     }
 
-    public double calculate(double num1, double num2, char operator) {
+    public double calculate(char operator) {
         return strategyFactory.getStrategy(operator).calculate(num1, num2);
+    }
+
+    public void erase() {
+
+    }
+
+    public void clear() {
+        num1 = 0;
+        num2 = 0;
+    }
+
+    public void sqrt() {
+
+    }
+
+    public void equals() {
+        
     }
 
     public boolean isValidExpression(String term) {
         if(term == null) throw new NullPointerException("Exp can't be null");
 
-        List<Character> expChars = term.chars().mapToObj(c -> (char) c).toList();
+        List<Character> termChars = term.chars().mapToObj(c -> (char) c).toList();
 
-        return expChars.stream().allMatch(c -> VALID_CHARACTERS.indexOf(c) >= 0) && !term.trim().isBlank();
+        return termChars.stream().allMatch(c -> VALID_CHARACTERS.indexOf(c) >= 0) && !term.trim().isBlank();
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
     }
 }
